7f0aae918c206428ba5a4e59d57114b37b32e655
mohsina-mou <mou.mohsinaakter@gmail.com>
2021-05-25 02:14:00 +0600

    add Education

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 var myDate = new Date();
 var hrs = myDate.getHours();
 
-if (hrs < 12) {
+if (hrs >= 3 && hrs < 12) {
     document.getElementById("greetings").innerHTML = "Good Morning";
 
 }
-else if (hrs >= 12 && hrs <= 17) {
+else if (hrs >= 12 && hrs < 17) {
     document.getElementById("greetings").innerHTML = "Good Afternoon";
 
 
 }
-else if (hrs >= 17 && hrs <= 24) {
-    document.getElementById("greetings").innerHTML = "Good Evening";
-}
+else if (hrs >= 17 && hrs < 24) {
+    document.getElementById("greetings").innerHTML = "Good Evening";}
+
+else if (hrs >= 0 && hrs < 3) {
+        document.getElementById("greetings").innerHTML = "Good Night";
+    }
